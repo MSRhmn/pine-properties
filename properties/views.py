437ebaf91b7a694c1d9138097cf50747cb989d1a
Md. Shamim Rahman <apubdtea@gmail.com>
2024-08-22 10:42:06 +0600

    feat: define views for the properties app

--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail
 
-# Create your views here.
+from .models import Property
+from .forms import ContactForm
+
+
+def home(request):
+    return render(request, "properties/home.html")
+
+
+def properties(request):
+    properties = Property.objects.all()
+    return render(request, "properties/properties.html", {"properties": properties})
+
+
+def contact(request):
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data["name"]
+            email = form.cleaned_data["email"]
+            message = form.cleaned_data["message"]
+
+            # The email sending logic
+            send_mail(
+                f"Contact Form Submission from {name}",
+                message,
+                email,
+                ["privateworkprovider@gmail.com"],
+            )
+            return redirect("home")
+    else:
+        form = ContactForm()
+
+    return render(request, "properties/contact.html", {"form": form})
