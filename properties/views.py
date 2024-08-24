622580c10973f1b3ba01bb2bd2455719d4172100
Md. Shamim Rahman <apubdtea@gmail.com>
2024-08-24 23:29:25 +0600

    update: email sending logic
    Fix the email sending logic with proper erro handling and hiding the
    credentials under environment variables.

--- properties/views.py
+++ properties/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
-from django.core.mail import send_mail
+from django.core.mail import EmailMessage
+from django.http import HttpResponse
+from django.conf import settings
 
 from .models import Property
 from .forms import ContactForm
@@ -23,12 +25,19 @@ def contact(request):
             message = form.cleaned_data["message"]
 
             # The email sending logic
-            send_mail(
-                f"Contact Form Submission from {name}",
-                message,
-                email,
-                ["privateworkprovider@gmail.com"],
+            email_message = EmailMessage(
+                subject=f"Contact Form Submission from {name}",
+                body=message,
+                from_email=settings.EMAIL_HOST_USER,
+                to=[settings.CONTACT_EMAIL],
+                reply_to=[email],
             )
+
+            try:
+                email_message.send()
+            except Exception as e:
+                return HttpResponse(f"Failed to send email: {e}")
+
             return redirect("properties:home")
     else:
         form = ContactForm()
