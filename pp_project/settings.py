9ab8deb09a26376b28ac95bb78289303be12422f
Md. Shamim Rahman <apubdtea@gmail.com>
2024-09-09 18:56:53 +0600

    chore: configure settings for media uploads

--- pp_project/settings.py
+++ pp_project/settings.py
@@ -138,3 +138,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = env("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
 CONTACT_EMAIL = env("CONTACT_EMAIL")
+
+# Configure of media files
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
