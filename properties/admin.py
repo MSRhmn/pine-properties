2ed2346f5cb74ba96241ddf6066087028bf443ec
Md. Shamim Rahman <apubdtea@gmail.com>
2025-08-13 13:19:47 +0600

    chore: fix the indentation in admin

--- properties/admin.py
+++ properties/admin.py
@@ -6,7 +6,15 @@ from .models import Property
 
 @admin.register(Property)
 class PropertyAdmin(admin.ModelAdmin):
-    list_display = ("title", "location", "price", "image_preview", "property_type", "listing_type", "is_available")
+    list_display = (
+        "title",
+        "location",
+        "price",
+        "image_preview",
+        "property_type",
+        "listing_type",
+        "is_available",
+    )
     search_fields = ("title", "location")
     list_filter = ("property_type", "listing_type", "is_available")
     readonly_fields = ("image_preview",)
