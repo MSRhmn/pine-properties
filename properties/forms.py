1024a17da08bfb526d1288a8c7d2fada78d0baf4
Md. Shamim Rahman <apubdtea@gmail.com>
2025-10-12 10:34:22 +0600

    feat: add phone option in forms
    Adding phone option in the forms feature, also updating all the form
    elements to show placeholder information.

--- properties/forms.py
+++ properties/forms.py
@@ -3,7 +3,25 @@ from django import forms
 
 class ContactForm(forms.Form):
     name = forms.CharField(
-        max_length=100, widget=forms.TextInput(attrs={"class": "form-control"})
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Your full name"}
+        ),
     )
-    email = forms.EmailField(widget=forms.EmailInput(attrs={"class": "form-control"}))
-    message = forms.CharField(widget=forms.Textarea(attrs={"class": "form-control"}))
+    email = forms.EmailField(
+        widget=forms.EmailInput(
+            attrs={"class": "form-control", "placeholder": "your@email.com"}
+        )
+    )
+    phone = forms.CharField(
+        max_length=20,
+        required=False,
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Phone (optional)"}
+        ),
+    )
+    message = forms.CharField(
+        widget=forms.Textarea(
+            attrs={"class": "form-control", "rows": 4, "placeholder": "Your message"}
+        )
+    )
