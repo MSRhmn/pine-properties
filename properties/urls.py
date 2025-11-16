5f61aa2f2566b6a1861bafa8b39c6cebc1babb3b
Md. Shamim Rahman <apubdtea@gmail.com>
2025-11-16 12:39:30 +0600

    chore: indent the properties url file

--- properties/urls.py
+++ properties/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path("properties/", views.properties, name="properties"),
     path("properties/<int:pk>/", views.property_detail, name="property_detail"),
     path("contact/", views.contact, name="contact"),  # General contact
-    path("contact/<int:property_id>/", views.contact, name="contact"),  # Property inquiry
+    path(
+        "contact/<int:property_id>/", views.contact, name="contact"
+    ),  # Property inquiry
 ]
