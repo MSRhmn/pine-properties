d51ccf0eb35429009756345459703797375ca243
Md. Shamim Rahman <apubdtea@gmail.com>
2025-11-27 12:49:48 +0600

    feat: update the contact url
    Updating the contact url to accept slug values.

--- properties/urls.py
+++ properties/urls.py
@@ -9,7 +9,5 @@ urlpatterns = [
     path("properties/", views.properties, name="properties"),
     path("properties/<slug:slug>/", views.property_detail, name="property_detail"),
     path("contact/", views.contact, name="contact"),  # General contact
-    path(
-        "contact/<int:property_id>/", views.contact, name="contact"
-    ),  # Property inquiry
+    path("contact/<slug:slug>/", views.contact, name="contact"),  # Property inquiry
 ]
