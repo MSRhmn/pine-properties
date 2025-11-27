7cbd906cab4990fd8c4245eec8b3ae7c55de7691
Md. Shamim Rahman <apubdtea@gmail.com>
2025-11-27 12:06:23 +0600

    update: properties url
    Updating property details url from pk to slug model.

--- properties/urls.py
+++ properties/urls.py
@@ -7,7 +7,7 @@ app_name = "properties"
 urlpatterns = [
     path("", views.home, name="home"),
     path("properties/", views.properties, name="properties"),
-    path("properties/<int:pk>/", views.property_detail, name="property_detail"),
+    path("properties/<slug:slug>/", views.property_detail, name="property_detail"),
     path("contact/", views.contact, name="contact"),  # General contact
     path(
         "contact/<int:property_id>/", views.contact, name="contact"
